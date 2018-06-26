ead6e4b481b1809a0e97f6dbe8d1b4fab2ae457f
Alejandro G <gear_one1@hotmail.com>
2018-06-26 19:47:07 -0300

    Nuevo Proyecto

---
+++ Proyecto/Prode.Core.Entidades/Partido.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prode.Core.Entidades
+{
+    public class Partido
+    {
+        public Equipo EquipoLocal { get; set; }
+        public Equipo EquipoVisitante { get; set; }
+        public int GolesLocal { get; set; }
+        public int GolesVisitante { get; set; }
+        public DateTime   Fecha{ get; set; }
+        public String InformarResultado {
+            get
+            {
+                var resultado = "";
+                if (GolesLocal>GolesVisitante)
+                {
+                    resultado = $"Gano {EquipoLocal.Nombre}";
+                }
+                else if (GolesLocal<GolesVisitante)
+                {
+                    resultado = $"Gano {EquipoLocal.Nombre}";
+                }
+                else
+                {
+                    resultado = "Empate";
+                }
+                return resultado;
+            }
+           
+
+            set; }
+
+
+    }
+}
